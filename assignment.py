47aa1a5fa41eb3b1d60f1de5b04472bd299356a8
Munkhdorj Munkhbaatar <64317250+munkhdorj-m@users.noreply.github.com>
2026-09-22 11:17:27 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,26 +1,12 @@
 # You can remove 'pass' if you written code in the function
 # Exercise 1
 
-def find_all_positions(data, target):
+def find_closest(data, target):
     # Write your code here
     pass
 
 # Exercise 2
-def find_student_by_id(records, student_id):
+def integer_sqrt(n):
     # Write your code here
     pass
 
-# Exercise 3
-def binary_search_steps(data, target):
-    # Write your code here
-    pass
-
-# Exercise 4
-def find_insert_position(data, value):
-    # Write your code here
-    pass
-
-# Exercise 5
-def first_and_last_position(data, target):
-    # Write your code here
-    pass
